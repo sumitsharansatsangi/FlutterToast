267aaa12f831fffa2eee8fb0f85afbdb5b898b6a
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-26 11:37:30 +0530

    code updated

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // Flutter plugin must come after Android/Kotlin plugins
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -19,10 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.fluttertoast_example"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 37
         versionCode = flutter.versionCode
@@ -31,8 +28,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
